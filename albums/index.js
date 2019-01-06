43ccdd11f6d473735c0685eaaa8e7a156c7d0fb0
Victor <vvteixeira02@gmail.com>
2019-01-06 23:35:50 +0900

    refactor to class component

--- albums/index.js
+++ albums/index.js
@@ -1,12 +1,16 @@
 
 // Import a library to create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header'; // importing js file do not need .js
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header'; // importing js file do not need .js
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-	<Header headerText={'Albums'} />
+	<View>
+		<Header headerText={'Albums'} />
+		<AlbumList />
+	</View>
 );
 
 // Render it into the device (must have the project's name)
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+	render() {
+		return (
+			<View>
+				<Text> fddsf </Text>
+			</View>
+		);	
+	}	
+}
+
+export default AlbumList;
