2170f80ca01a0e4c86354e8951fc58ba569f12d0
Victor <vvteixeira02@gmail.com>
2019-01-15 00:05:11 +0900

    Added album images to list

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,16 +1,67 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
-const AlbumDetail = (props) => {
+function AlbumDetail({ album }) {
+	const { 
+		title, 
+		artist, 
+		thumbnail_image, 
+		image 
+	} = album;
+	const { 
+		thumbnailStyle, 
+		headerContentStyle,
+		thumbnailContainerStyle,
+		headerTextStyle,
+		imageStyle
+	} = styles;
+
 	return (
-		<Card>
+		<Card> 
 			<CardItem>
-				<Text>{props.album.title}</Text>
+				<View style={thumbnailContainerStyle}>
+					<Image
+						style={thumbnailStyle} 
+						source={{ uri: thumbnail_image }} 
+					/>
+				</View>
+				<View sytle={headerContentStyle}>
+					<Text style={headerTextStyle}>{title}</Text>
+					<Text>{artist}</Text>	
+				</View> 
+			</CardItem>
+			<CardItem>
+				<Image style={imageStyle} source={{ uri: image }} />
 			</CardItem>
 		</Card>
 	);
+}
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	headerTextStyle: {
+		fontSize: 18
+	},
+	thumbnailStyle: {
+		height: 50,
+		width: 50
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+	},
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
+	}
 };
 
 export default AlbumDetail;
