7a0ae529f983a65d8b3df5847c83dd510bc62d8b
Victor <vvteixeira02@gmail.com>
2019-01-21 00:36:42 +0900

    More redux code to manager

--- manager/src/App.js
+++ manager/src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from '@firebase/app';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     componentWillMount() {
@@ -22,11 +23,7 @@ class App extends Component {
     render() {
         return (
             <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>
-                        Test
-                    </Text>
-                </View>
+                <LoginForm />
             </Provider>
         );
     }
--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import { Card } from './common';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
+	onEmailChange(text) {
+		this.props.emailChanged(text);
+	}
+
     render() {
         return (
             <Card>
-                
+                <CardSection>
+                	<Input
+                		label="Email"
+                		placeholder="email@gmail.com"
+                		onChangeText={this.onEmailChange.bind(this)}
+                		value={this.props.email}
+                	/>
+                </CardSection>
+                <CardSection>
+                	<Input 
+                		secureTextEntry
+                		label="Password"
+                		placeholder="password"
+                	/>
+                </CardSection>
+                <CardSection>
+                	<Button>
+                		Login
+                	</Button>
+                </CardSection>
             </Card>
         );
     }
 }
 
-export default LoginForm
+const mapStateToProps = state -> {
+	return {
+		email: state.auth.email
+	};
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
