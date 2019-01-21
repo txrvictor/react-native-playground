e4993303f1564af50724bfd84e2729377ac1ef29
Victor <victor@adenasoft.com>
2019-01-21 18:39:11 +0900

    Added navigation to manager proj

---
+++ manager/src/components/EmployeeList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>dsfsdsfdfsdf</Text>
+                <Text>dsfsdsfdfsdf</Text>
+                <Text>dsfsdsfdfsdf</Text>
+                <Text>dsfsdsfdfsdf</Text>
+                <Text>dsfsdsfdfsdf</Text>
+            </View>
+        );
+    }
+}
+
+export default EmployeeList;
--- manager/src/components/common/CardSection.js
+++ manager/src/components/common/CardSection.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 
 const CardSection = (props) => {
+	// overriding styles (our own internal style "containerStyle") with
+	// the style given in props
 	return (
-		<View style={styles.containerStyle}>
+		<View style={[styles.containerStyle, props.style]}>
 			{props.children}
 		</View>
 	); 
